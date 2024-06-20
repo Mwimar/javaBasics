3436a850c53227437397ff7d7b1cc36e25e1114e
Mark Mwima <markmwimar@gmail.com>
2024-06-20 21:06:16 +0300

    updated age variable

--- Base.java
+++ Base.java
@@ -2,6 +2,7 @@ public class Base{
        static int age;
     public static void main(String[] args){
        age=26;
+       age=30;
         System.out.println("I am "+ age+ " years old");
     }
 }
