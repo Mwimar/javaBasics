406c6ba5d49d460e6402d387e56c231b5115779d
Mark Mwima <markmwimar@gmail.com>
2024-06-21 00:50:58 +0300

    Strings

--- Base.java
+++ Base.java
@@ -14,9 +14,19 @@ public class Base{
         System.out.println(isStudent && isLibraryMember);
 
         int dig1=5;
-        System.out.println(dig1++);
-        System.out.println(dig1);
-        System.out.println(++dig1);
+//        System.out.println(name);
+        String name= "Mwima";
+        String work="Software Engineer";
+        int exp=5;
+        String formattedText=String.format("%s is a %s with %d years experience", name,work,exp);
+        System.out.println(formattedText);
+        String str1=new String("xxl");
+        String str2=new String("xxl");
+        String str3=new String("XXL");
+        System.out.println(str1==str2);
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equalsIgnoreCase(str3));
+
 
     }
 }
