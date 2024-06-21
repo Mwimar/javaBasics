f6742c234394a997945703d02499a61a3010ec11
Mark Mwima <markmwimar@gmail.com>
2024-06-21 22:01:51 +0300

    if condition

--- Conditions.java
+++ Conditions.java
@@ -7,15 +7,25 @@ public class Conditions{
 
         System.out.print("Enter First number:");
         double numbr1=scanner.nextDouble();
+        scanner.nextLine();
 
         System.out.print("Enter Second number:");
         double numbr2=scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("What operation do you want to perform?");
+        String operation=scanner.nextLine();
+
+        if(operation.equals("sum")){
+            System.out.printf("%f+%f=%f", numbr1, numbr2, numbr1+numbr2);
+        }
 
 
 
-        System.out.println(numbr1);
-        System.out.println(numbr2);
-        System.out.println(numbr1*numbr2);
+
+
+
+        scanner.close();
 
     }
 }
