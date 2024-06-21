cd1fabf432ddb6ee912144cd5d5d8266f52302f5
Mark Mwima <markmwimar@gmail.com>
2024-06-21 20:28:19 +0300

    importing scanner

--- User.java
+++ User.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class User{
     public static void main(String[] args){
         String stats="I am a backend developer";
+        Scanner scanner=new Scanner(System.in);
 
     }
 }
