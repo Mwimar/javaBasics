2b254c0997ceb9953ae12d8c9ce3090994317f5e
Mark Mwima <markmwimar@gmail.com>
2024-06-21 20:40:33 +0300

    printing on same line

--- User.java
+++ User.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class User{
     public static void main(String[] args){
-        String stats="I am a backend developer";
         Scanner scanner=new Scanner(System.in);
 
-        System.out.println("Howdy ?");
-
+        System.out.print("Howdy ?");
        String newStart= scanner.nextLine();
-        System.out.println(newStart);
+
+        System.out.print(newStart);
+
         scanner.close();
 
     }
