9d49704afb01e4fafbc707ef8ce8bcffbe06e372
Mark Mwima <markmwimar@gmail.com>
2024-06-21 21:46:39 +0300

    using doubles

---
+++ Conditions.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Conditions{
+    public static void main(String[] args){
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Enter First number:");
+        double numbr1=scanner.nextDouble();
+
+        System.out.print("Enter Second number:");
+        double numbr2=scanner.nextDouble();
+
+
+
+        System.out.println(numbr1);
+        System.out.println(numbr2);
+        System.out.println(numbr1*numbr2);
+
+    }
+}
