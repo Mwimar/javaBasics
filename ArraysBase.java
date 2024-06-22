9e82ca5f4b54307dcb0611d568fcbb1f9061311c
Mark Mwima <markmwimar@gmail.com>
2024-06-22 18:37:40 +0300

    refactoring arrays

--- ArraysBase.java
+++ ArraysBase.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
 public class ArraysBase {
     public static void main (String[] args){
-        char vowels[]= new char[5];
-
-        vowels[0]='a';
-        vowels[1]='e';
-        vowels[2]='i';
-        vowels[3]='o';
-        vowels[4]='u';
+//        char vowels[]= new char[5];
+//
+//        vowels[0]='a';
+//        vowels[1]='e';
+//        vowels[2]='i';
+//        vowels[3]='o';
+//        vowels[4]='u';
+        char vowels[] = {'a', 'e', 'i', 'o', 'u'};
 
         System.out.println(vowels[1]);
         System.out.println(Arrays.toString(vowels));
