cc7639af9661b99abaa9eb417f5aca47b7cb5201
Mark Mwima <markmwimar@gmail.com>
2024-06-22 18:26:42 +0300

    individual array value

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays{
+    public static void main (String[] args){
+        char vowels[]= new char[5];
+
+        vowels[0]='a';
+        vowels[1]='e';
+        vowels[2]='i';
+        vowels[3]='o';
+        vowels[4]='u';
+
+        System.out.println(vowels[1]);
+    }
+}
