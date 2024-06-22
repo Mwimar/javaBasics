6730a3c27a9712b1a5c4a89054041f26d36c0f3a
Mark Mwima <markmwimar@gmail.com>
2024-06-22 22:25:45 +0300

    adding up array values

--- Loops.java
+++ Loops.java
@@ -1,9 +1,11 @@
 public class Loops{
     public static void main(String[] args){
+        int sum=0;
         int numbers[] = {1,2,3,4,5,6,7,8,9};
-        for(int index=1; index<numbers.length; index++){
-            System.out.println(numbers[index]);
+        for(int index=0; index<numbers.length; index++){
+            sum+=numbers[index];
         }
-        System.out.println(numbers.length);
+            System.out.println(sum);
+
     }
 }
