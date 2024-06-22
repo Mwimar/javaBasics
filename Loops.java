2eee7ccebda2287c1fe490bd76bb88b3141def7b
Mark Mwima <markmwimar@gmail.com>
2024-06-22 22:39:28 +0300

    nested for loop

--- Loops.java
+++ Loops.java
@@ -1,11 +1,17 @@
 public class Loops{
     public static void main(String[] args){
-        int sum=0;
-        int numbers[] = {1,2,3,4,5,6,7,8,9};
-        for(int index=0; index<numbers.length; index++){
-            sum+=numbers[index];
-        }
-            System.out.println(sum);
+//        int sum=0;
+//        int numbers[] = {1,2,3,4,5,6,7,8,9};
+//        for(int index=0; index<numbers.length; index++){
+//            sum+=numbers[index];
+//        }
+
+        for(int number=1; number<=10; number++){
+            for(int multiplier=1; multiplier<=10; multiplier++){
+                System.out.printf("%d X %d = %d \n", number,multiplier, number*multiplier);
+            }
+
+                   }
 
     }
 }
