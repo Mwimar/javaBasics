4ead6b9648d93e3acf068aad5e191480c0fabdf4
Mark Mwima <markmwimar@gmail.com>
2024-06-23 15:48:17 +0300

    do while loop

--- Loops.java
+++ Loops.java
@@ -28,10 +28,14 @@ public class Loops{
         int number=5;
         int multiplier=1;
 
-        while(multiplier<=10){
-            System.out.printf("%d X %d = %d \n", number, multiplier, number*multiplier);
+//        while(multiplier<=10){
+//            System.out.printf("%d X %d = %d \n", number, multiplier, number*multiplier);
+//            multiplier++;
+//        }
+
+        do{ System.out.printf("%d X %d = %d \n", number, multiplier, number*multiplier);
             multiplier++;
-        }
+        }while(multiplier<=10);
 
     }
 }
