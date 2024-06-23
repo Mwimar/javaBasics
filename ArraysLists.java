c68905ac437f160fc43019dd2cb704f0deb9ec64
Mark Mwima <markmwimar@gmail.com>
2024-06-23 17:20:21 +0300

    checking array size

--- ArraysLists.java
+++ ArraysLists.java
@@ -18,6 +18,7 @@ public class ArraysLists{
         numbers.sort(Comparator.naturalOrder()); //Sorting arrays;
         numbers.sort(Comparator.reverseOrder()); // sorting arrays in reverse order;
         System.out.println(numbers.toString());
+        System.out.println(numbers.size());
 
 //        System.out.println(numbers.get(3).toString());
 //        System.out.println(numbers.get(3));
