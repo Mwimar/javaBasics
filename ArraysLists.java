53be1ff4ea75420e95cb13d32ec502a88a5a76c1
Mark Mwima <markmwimar@gmail.com>
2024-06-23 17:16:48 +0300

    sorting arrays in natural order

--- ArraysLists.java
+++ ArraysLists.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 
 public class ArraysLists{
     public static void main(String[] args){
         ArrayList<Integer> numbers= new ArrayList<Integer>();
-        numbers.add(1);
-        numbers.add(2);
         numbers.add(3);
-        numbers.add(4);
+        numbers.add(1);
         numbers.add(5);
+        numbers.add(4);
+        numbers.add(2);
 //        numbers.remove(3);
 
 //        numbers.remove(Integer.valueOf(3)); // removing an array value
 //        numbers.clear();//clearing an array;
-        numbers.set(2, Integer.valueOf(55));//updating an array value;
+//        numbers.set(2, Integer.valueOf(55));//updating an array value;
+        numbers.sort(Comparator.naturalOrder()); //Sorting arrays;
         System.out.println(numbers.toString());
 
 //        System.out.println(numbers.get(3).toString());
