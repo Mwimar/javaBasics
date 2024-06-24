eecf808224926ddaa0ae88f49ed6a7e53bf28b6a
Mark Mwima <markmwimar@gmail.com>
2024-06-24 22:11:08 +0300

    updating values in for each method

--- HashMaps.java
+++ HashMaps.java
@@ -20,7 +20,9 @@ public class HashMaps{
 //        System.out.println(examScores.containsValue(Integer.valueOf(81)));
 //        System.out.println(examScores.isEmpty());
         examScores.forEach((subject,score)->{
-            System.out.println(subject+ " "+ score);
+//            System.out.println(subject+ " "+ score);
+            examScores.replace(subject,score-20);
         });
+        System.out.println(examScores.toString());
     }
 }
