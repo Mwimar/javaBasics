59b7e4fe397512afc17fcc33bb8f3da5878a1c9a
Mark Mwima <markmwimar@gmail.com>
2024-06-24 21:27:03 +0300

    initializing a Hash map

--- HashMaps.java
+++ HashMaps.java
@@ -2,5 +2,7 @@ import java.util.HashMap;
 public class HashMaps{
     public static void main(String[] args){
 
+        HashMap<String,Integer> examScores= new HashMap<String,Integer>()
+
     }
 }
