5b585f3d8a7347f1faae945e7f003917f6b624d5
Mark Mwima <markmwimar@gmail.com>
2024-06-24 21:33:23 +0300

    printing hashmap values

--- HashMaps.java
+++ HashMaps.java
@@ -2,10 +2,12 @@ import java.util.HashMap;
 public class HashMaps{
     public static void main(String[] args){
 
-        HashMap<String,Integer> examScores= new HashMap<String,Integer>()
+        HashMap<String,Integer> examScores= new HashMap<String,Integer>();
 
         examScores.put("Biology", 90);
         examScores.put("Chemistry", 79);
         examScores.put("Physics", 85);
+
+        System.out.println(examScores.toString());
     }
 }
