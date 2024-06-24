d75c4882e789e0f26a05284c8c2fa706c3f79fd4
Mark Mwima <markmwimar@gmail.com>
2024-06-24 21:42:09 +0300

    updating individual  hashmap item value

--- HashMaps.java
+++ HashMaps.java
@@ -7,7 +7,10 @@ public class HashMaps{
         examScores.put("Biology", 90);
         examScores.put("Chemistry", 79);
         examScores.put("Physics", 85);
+        examScores.putIfAbsent("Physics", 68);// replaces if value is absent;
+        examScores.replace("Biology",81);
 
+        System.out.println(examScores.getOrDrfault("Kiswahili",76));;
         System.out.println(examScores.toString());
         System.out.println(examScores.get("Chemistry"));// printing individual item
     }
