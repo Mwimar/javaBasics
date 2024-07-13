86a9739ee0fb2f14f2d046076c0143731b31bedc
Mark Mwima <markmwimar@gmail.com>
2024-07-13 14:11:59 +0300

    printing

--- practice.java
+++ practice.java
@@ -3,13 +3,22 @@ public class practice {
     public static void main(String[] args){
 
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter length? ");
-        int length= scanner.nextInt();
+//    System.out.print("Enter length? ");
+//        int length= scanner.nextInt();
+//
+//         System.out.print(" Enter width?");
+//         int width = scanner.nextInt();
+//         System.out.printf("The Area is calculated by multiplying the length %d by width %d which equals " +(length*width)+ "squared ",length,width );
+//
+//scanner.close();
+int age=3;
+//for (int i=0; i<=8; i++){
+//    System.out.println(i);
+//}
+        for(int i=1; i<=5; i++){
 
-         System.out.print(" Enter width?");
-         int width = scanner.nextInt();
-         System.out.printf("The Area is calculated by multiplying the length %d by width %d which equals " +(length*width)+ "squared ",length,width );
+            System.out.println(i +" - "+ (i*i));
+        }
 
-scanner.close();
     }
 }
